8f388f2c535eaf9e59940e582a71e60281980157
YalinA <YalinA@YalinA-Laptop.dm.cx>
2019-01-29 15:05:54 +0200

    jjjj

--- CouponSystem/src/Main/Test.java
+++ CouponSystem/src/Main/Test.java
@@ -1,33 +1,26 @@
 package Main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import DB.Database;
 import Facade.CompanyFacade;
 import JavaBeans.Company;
 import JavaBeans.Coupon;
 import JavaBeans.CouponType;
 import JavaBeans.Customer;
 
-
-
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
-	
-	    Company company = new Company(1,"Amdocs","1234", "oriel@test.com");
-		Coupon coupon = new Coupon(1,"The eucalyptus", Utils.getDate(), Utils.getDate() , 1245, CouponType.RESTURANTS , "wtf" , 1251, "image" );
-		Customer customer = new Customer(123,"Oriel","1234");
-		
+
+		Company company = new Company(1, "Amdocs", "1234", "oriel@test.com");
+		Coupon coupon = new Coupon(1, "The eucalyptus", Utils.getDate(), Utils.getDate(), 1245, CouponType.RESTURANTS,
+				"wtf", 1251, "image");
+		Customer customer = new Customer(123, "Oriel", "1234");
 
 		CompanyFacade companyFacade = new CompanyFacade();
-		
-        companyFacade.removeCompany(company);
-		 
-		
-		
+
+		companyFacade.removeCompany(company);
+
 	}
 
 }
